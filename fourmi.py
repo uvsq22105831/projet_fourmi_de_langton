64c464c31c65490902e589c2db885520d94081e0
«Yacine <akli-yacine.rafil@ens.uvsq.fr>
2022-04-13 17:01:40 +0200

    chaise

--- fourmi.py
+++ fourmi.py
@@ -3,3 +3,9 @@
 #JOUAN Maxime
 #https://github.com/uvsq22105831/projet_fourmi_de_langton
 ########################################
+import tkinter as tk
+fourmi = tk.Tk()
+fourmi.title("fourmi de Langton")
+can = tk.Canvas(fourmi, width=LARGEUR, height=HAUTEUR)
+can.grid(column=1, row=0)
+bouton = tk.Button(fourmi,text="play")
