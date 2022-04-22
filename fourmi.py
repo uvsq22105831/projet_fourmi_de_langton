b39ff38aece8eb25c972fe1dba0deed7c884f105
Maxime <maxime.jouan1@ens.uvsq.fr>
2022-04-22 13:37:04 +0200

    fourmi+bouton pause

--- fourmi.py
+++ fourmi.py
@@ -5,42 +5,79 @@
 ########################################
 import tkinter as tk
 
-LARGEUR = 500
-HAUTEUR = 500
-NOMBRECARRE=80
+
+LARGEUR = 700
+HAUTEUR = 700
+NOMBRECARRE=95
 Cellule =LARGEUR // NOMBRECARRE
+direction= (1,0)
+LPOS=LARGEUR//Cellule
+tempsetape =100
+position =(LPOS//2-10,LPOS//2+19)
+sol = [[0] * LPOS for _ in range(LPOS)]
+
 
 #########################
 #fonctions
-def fairesol():
-    """créé le sol du fait que si la case est de valeur 1
-    cela représente une case noir. Et si la case est de 
-    valeur 0 cela représente une case blanche"""
-    global grille, sol
-    grille =[]
-    sol =[]
-    for i in range(Cellule):
-        sol.append([0]*Cellule)
-        grille.append([0]*Cellule)
-          
-    for i in range(Cellule):
-        for j in range(Cellule):
-            largeur = LARGEUR // Cellule
-            hauteur = HAUTEUR // Cellule
-            x0, y0 = i * largeur, j * hauteur
-            x1, y1 = (i + 1) * largeur, (j + 1) * hauteur
-            rectangle =can.create_rectangle((x0, y0), (x1, y1), fill="white",outline="")
 
 
 
+def deplacement(position,direction, sol):
+    """créé le déplacement a partir de la liste sol"""
+    i, j = position
+    a, b = direction
+    aa, bb = (b, -a) if sol[i][j] == 0 else (-b, a)
+    return (i + aa, j + bb), (aa, bb)
 
+def dessine(position, direction, sol):
+    """va donner la nouvelle direction et position de la fourmi"""
+    (ii, jj), nouvelledir = deplacement(position, direction, sol)
+    i, j = position
+    colorcarre = sol[i][j]
+
+    if colorcarre == 0:
+        colorcarre = creesol(i, j)
+        sol[i][j] = colorcarre
+    else:
+        can.delete(colorcarre)
+        sol[i][j] = 0
+
+    return (ii, jj), nouvelledir
+
+def creesol(i, j):
+    x, y = i * Cellule, j * Cellule
+    colorcarre = can.create_rectangle((x, y), (x +Cellule , y + Cellule),fill="black",outline='')
+    return colorcarre
+
+def etape():
+    global position, direction
+    position, direction = dessine(position, direction, sol)
+    fourmi.after(tempsetape, etape)
+
+def fonctionboutplay():
+    pause()
+    changebout()
+
+def pause():
+    global tempsetape
+    if tempsetape == 9999999999:
+        tempsetape =100
+        fourmi.after(tempsetape, etape)
+    else:
+        tempsetape =9999999999
+
+def changebout():
+    if bouton_play['text'] =="pause":
+        bouton_play['text']="play"
+    else:
+        bouton_play['text']="pause"
 ##########################################
 #programme principal
 fourmi = tk.Tk()
 fourmi.title("fourmi de Langton")
 can = tk.Canvas(fourmi, width= LARGEUR, height=HAUTEUR)
-bouton_play = tk.Button(fourmi,text="continuer")
-fairesol()
+bouton_play = tk.Button(fourmi,text="pause",command= fonctionboutplay)
 can.grid(column=1, row=0)
-bouton_play.grid(column=1,row=1)
+etape()
+bouton_play.grid(column=1,row=2)
 fourmi.mainloop()
